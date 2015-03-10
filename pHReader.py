d0b22f7eb660066cd99d37b0c13970baebb18fe6
Evan Galpin <egalpin@uwo.ca>
2015-03-10 14:15:30 -0400

    Restructured classes and corresponding files

---
+++ pHReader.py
@@ -0,0 +1,5 @@
+import Adafruit.I2C as I2C
+#define ADDRESS 0x4D # MCP3221 A5 in Dec 77 A0 = 72 A7 = 79)
+                     # A0 = x48, A1 = x49, A2 = x4A, A3 = x4B, 
+                     # A4 = x4C, A5 = x4D, A6 = x4E, A7 = x4F
+
